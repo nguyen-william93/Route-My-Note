adfdccc6fdca9593f356b5c27950f591296b7a2e
nguyen-william93 <=>
2021-10-24 16:00:49 -0500

    add lib folder and update the note.js to contain all the necessary function

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,18 +1,28 @@
 const router = require('express').Router();
 const notes  = require("../../db/db.json");
+const { createNewNote, validateNote, findNoteById, deleteNote } = require('../../lib/note');
+const { v1: uuidv1, v4: uuidv4 } = require("uuid");
 
 router.get("/notes", (req, res) => {
     let result = notes;
-    console.log(result);
     res.json(notes);
 })
 
 router.post("/notes", (req, res) => {
-    req.body.id = notes.length.toString();
+    req.body.id = uuidv4();
 
-    // const note = createNewNote(req.body, notes);
+    if (!validateNote(req.body)){
+        res.status(404).send("The note is not properly formatted");
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
 
-
+router.delete("/notes/:id", (req, res) => {
+    let note = findNoteById(req.params.id, notes);
+    deleteNote(note, notes);
+    res.json(note);
 })
 
 
