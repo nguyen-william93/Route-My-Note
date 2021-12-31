a9dfe127109f62853c4bc0e6f93887ffb06d49f6
nguyen-william93 <=>
2021-12-30 22:58:09 -0600

    fix the delete route

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -23,8 +23,11 @@ router.post("/notes", (req, res) => {
 
 //delete route, delete note base on ID passed in
 router.delete("/notes/:id", (req, res) => {
+    // find the id
     let result = findNoteById(req.params.id, notes);
-    deleteNote(note, notes);
+    //passing in the id and the notes array
+    deleteNote(result, notes);
+    //return the updated note list
     res.json(notes);
 })
 
